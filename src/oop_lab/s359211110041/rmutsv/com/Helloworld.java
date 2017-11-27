62207d320cd0a3a78cd360e059bbd1a258248280
amon <amon4254@gmail.com>
2017-11-27 14:02:25 +0700

    Initial commit

---
+++ src/oop_lab/s359211110041/rmutsv/com/Helloworld.java
@@ -0,0 +1,11 @@
+package oop_lab.s359211110041.rmutsv.com;
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world, I'm priaw");
+        System.out.println("My name is amon sirikhamhom");
+
+//        Add infomation
+        System.out.println("Faculty: Management Technology, RMUTXV");
+    } // main
+} // class
